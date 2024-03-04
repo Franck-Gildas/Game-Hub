c1d2fd9032f84ffce0a9d961677895440f10b89c
Franck Gildas <efmukamk01@monccnb.ca>
2024-03-04 15:43:27 -0400

    Refactor the Text component for displaying the error: removed duplicated SortSelector.tsx

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <text>{error}</text>;
+  if (error) return <Text>{error}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
